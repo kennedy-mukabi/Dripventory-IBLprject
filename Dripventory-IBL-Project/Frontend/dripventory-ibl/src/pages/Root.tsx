ef085a0089efe6be2168d7636d38a054005f9a0c
kenneth mukabi <kenkingmukabi@gmail.com>
2023-01-08 18:09:42 +0300

    Add files via upload

---
+++ Dripventory-IBL-Project/Frontend/dripventory-ibl/src/pages/Root.tsx
@@ -0,0 +1,119 @@
+import React, { FunctionComponent, useEffect } from "react";
+import { Outlet, useNavigate } from "react-router-dom";
+import {
+  AppBar,
+  Box,
+  Button,
+  Drawer,
+  MenuItem,
+  Toolbar,
+  Typography,
+  useTheme,
+} from "@mui/material";
+import { useAppContext } from "../context/AppContext";
+import { RowBox } from "../components";
+
+interface OwnProps {}
+
+type Props = OwnProps;
+export const destinations = [
+  { label: "Products", route: "/admin/products", selectedIndex: "products" },
+  { label: "Sales", route: "/admin/sales", selectedIndex: "sales" },
+  {
+    label: "Deliveries",
+    route: "/admin/deliveries",
+    selectedIndex: "deliveries",
+  },
+  { label: "Customers", route: "/admin/customers", selectedIndex: "customers" },
+];
+const Root: FunctionComponent<Props> = (props) => {
+  const navigate = useNavigate();
+  const { selectedIndex } = useAppContext();
+  const theme = useTheme();
+  useEffect(() => {
+    const loginStatus = sessionStorage.getItem("loginStatus");
+    if (loginStatus !== "true") {
+      navigate("/");
+    }
+  }, []);
+  const DeskDrawer = (
+    <Drawer
+      variant={"permanent"}
+      open={true}
+      anchor={"left"}
+      sx={{
+        width: "240px",
+        flexShrink: 0,
+        "& .MuiDrawer-paper": { width: "240px", boxSizing: "border-box" },
+      }}
+    >
+      <Toolbar sx={{ backgroundColor: `${theme.palette.primary.main}` }}>
+        {" "}
+        <Typography
+          variant={"h4"}
+          color={"white"}
+          fontWeight={"bold"}
+          sx={{ cursor: "pointer" }}
+          onClick={() => {
+            navigate("/admin/dashboard");
+          }}
+        >
+          Dripventory
+        </Typography>{" "}
+      </Toolbar>
+      <MenuItem
+        selected={selectedIndex === "dashboard"}
+        onClick={() => {
+          navigate("/admin/dashboard");
+        }}
+      >
+        {" "}
+        Dashboard
+      </MenuItem>
+      {destinations.map((destination) => (
+        <MenuItem
+          selected={selectedIndex === destination.selectedIndex}
+          key={destination.label}
+          onClick={() => {
+            navigate(destination.route);
+          }}
+        >
+          {" "}
+          {destination.label}{" "}
+        </MenuItem>
+      ))}
+      <RowBox sx={{ width: "100%", mt: "auto", mb: "5%" }}>
+        <Button
+          sx={{
+            width: "80%",
+            borderRadius: "10px",
+            backgroundColor: "white",
+            color: "black",
+            border: "4px solid black",
+          }}
+          variant={"text"}
+          onClick={() => {
+            sessionStorage.setItem("loginStatus", "false");
+            navigate("/");
+          }}
+        >
+          Log out
+        </Button>
+      </RowBox>
+    </Drawer>
+  );
+
+  return (
+    <Box sx={{ display: "flex", width: "100%", height: "100%" }}>
+      {DeskDrawer}
+      <Box sx={{ flexGrow: 1 }}>
+        <Box sx={{ width: "100%", height: "100%" }}>
+          {" "}
+          <Outlet />{" "}
+        </Box>
+      </Box>
+    </Box>
+  );
+};
+
+export default Root;
